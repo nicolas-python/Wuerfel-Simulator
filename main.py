abc2f67662a44452b3c49f3fc400c8698c55bc86
Nicolas <nicolasmader001@gmail.com>
2026-03-12 09:00:45 +0100

    Würfel bilder erstellt

--- main.py
+++ main.py
@@ -2,11 +2,59 @@
 
 import random
 
+
+dice1_bild =  """
++-----+
+|     |
+|  o  |
+|     |
++-----+
+"""
+
+dice2_bild =  """
++-----+
+| o   |
+|     |
+|   o |
++-----+
+"""
+
+dice3_bild = """
++-----+
+| o   |
+|  o  | 
+|  o  |
++-----+
+"""
+
+dice4_bild = """
++-----+
+| o o |
+|     |
+| o o |
++-----+
+"""
+
+dice5_bild = """
++-----+
+| o o |
+|  o  |
+| o o |
++-----+
+"""
+
+dice6_bild = """
++-----+
+| o o |
+| o o |
+| o o |
++-----+
+"""
+
 #funktionen
 def zufalls_zahl():
      dice = random.randint(1,6)          #randint spare ich mir die liste
      return dice                               #dice = würfel auf englisch
-
 
 while True:
 
@@ -19,6 +67,37 @@ while True:
     gesamt=dice1 + dice2
     print("Gesamt würde:",gesamt,"gewürfelt")
 
+#Würfel anzeigen
+    print("Würfel 1")
+
+    match dice1:
+        case 1 :
+            print(dice1_bild)
+        case 2:
+            print(dice2_bild)
+        case 3:
+            print(dice3_bild)
+        case 4 :
+            print(dice4_bild)
+        case 5:
+            print(dice5_bild)
+        case 6:
+            print(dice6_bild)
+
+    match dice2:
+        case 1 :
+            print(dice1_bild)
+        case 2:
+            print(dice2_bild)
+        case 3:
+            print(dice3_bild)
+        case 4 :
+            print(dice4_bild)
+        case 5:
+            print(dice5_bild)
+        case 6:
+            print(dice6_bild)
+
     weiter = input("Nochmal Würfeln? (y/n):")
 
     if weiter == "y":
@@ -26,3 +105,5 @@ while True:
 
     else:
         break
+
+
